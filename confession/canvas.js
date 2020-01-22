1e945cd02cd70d88145a2b18f3316bc3733dff0d
shantanuaryan67 <shantanuaryan67@gmail.com>
2020-01-22 23:34:30 +0530

    added tt
    canvas multi line support
    
    todo:
       autobreak superlong words like aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
       restrict length of confession

--- confession/canvas.js
+++ confession/canvas.js
@@ -1,14 +1,23 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 ctx.font = "60px monospace";
+lineheight = eval(getComputedStyle(document.documentElement).getPropertyValue('--height').replace("v", "*").replace("w", "window.innerWidth").replace("h", "window.innerHeight")) / 100 * 60 / 2000 * 2.7; //      fontsize + linespacing
+alert(lineheight);
+var offset;
 
 function update() {
+  offset = 0;
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   ctx.fillStyle = document.getElementById('bgcolor').value;
   ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   ctx.font = "60px monospace";
   ctx.fillStyle = document.getElementById('textcolor').value;
-  ctx.fillText(document.getElementById('maintext').value, 200, 300, 1200);
+  var maintext = document.getElementById('maintext').value.split(/\n/g);
+  for (var n = 0; n < maintext.length; n++) {
+    wrapText(ctx, maintext[n], 200, (300 + offset * lineheight), 1200, lineheight);
+    offset += 1;
+  }
+  // ctx.fillText(document.getElementById('maintext').value, 200, 300, 1200);
   ctx.font = "50px monospace";
   ctx.fillText(document.getElementById('sign').value, 200, 1700, 800);
   ctx.font = "40px monospace";
@@ -27,4 +36,24 @@ function getdataurl() {
   var dataURL = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
   console.log(dataURL);
   alert(dataURL);
+}
+
+function wrapText(context, text, x, y, maxWidth, lineHeight) {
+  var words = text.split(" ");
+  var line = "";
+  for (var n = 0; n < words.length; n++) {
+    var testLine = line + words[n] + " ";
+    var metrics = context.measureText(testLine);
+    var testWidth = metrics.width;
+
+    if (testWidth > maxWidth) {
+      context.fillText(line, x, y, maxWidth);
+      line = words[n] + " ";
+      y += lineHeight;
+      offset += 1;
+    } else {
+      line = testLine;
+    }
+  }
+  context.fillText(line, x, y, maxWidth);
 }
